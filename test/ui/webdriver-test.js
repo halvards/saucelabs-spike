eed691cf60615eb384cca8ff593a6bfc190fa338
Halvard Skogsrud <halvard@skogsrud.com>
2016-01-10 22:02:44 +1100

    Start application before UI tests

--- test/ui/webdriver-test.js
+++ test/ui/webdriver-test.js
@@ -5,16 +5,22 @@ const test = require('tape');
 
 const browser = webdriverio.remote({
   desiredCapabilities: {
-    browserName: 'phantomjs'
+    browserName: process.env.BROWSER || 'phantomjs'
   }
 });
 
+const baseUrl = `http://localhost:${process.env.PORT || 3000}`;
+
+console.log(`Testing application on ${baseUrl}`);
+
 test('should visit application', (assert) => {
   assert.plan(1);
-  browser.init()
-    .url('http://localhost:3000/')
-    .getTitle().then((title) => {
-      assert.equal(title, 'saucelabs-spike');
-    })
-    .end();
+  browser
+      .init()
+      .url(baseUrl)
+      .getTitle()
+      .then((title) => {
+        assert.equal(title, 'saucelabs-spike');
+      })
+      .end();
 });
